fcb0527f40ae81745aff9292f2e4564fe80a4d8c
Koen Klaren <koen.klaren@gmail.com>
2019-12-19 14:57:02 +0100

    Add infinite_array class

---
+++ util/infinite_array.h
@@ -0,0 +1,18 @@
+template<class value_t, class map_t = std::unordered_map<size_t, value_t>>
+class infinite_array {
+    map_t data;
+
+public:
+    value_t& at(size_t key) {
+        if (!data.count(key)) {
+            data.emplace(key, 0);
+        }
+        return data.at(key);
+    }
+    const value_t at(size_t key) const {
+        if (!data.count(key)) {
+            return 0;
+        }
+        return data.at(key);
+    }
+};
